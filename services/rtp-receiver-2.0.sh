196398723c091d6719070dcdbd07efd8273b2202
Jerry1098 <32243567+Jerry1098@users.noreply.github.com>
2025-11-18 22:54:46 +0000

    Convert rtp-receiver service to a shell script

--- services/rtp-receiver-2.0.sh
+++ services/rtp-receiver-2.0.sh
@@ -1,13 +1,8 @@
-[Service]
-Type=simple
-Restart=always
-RestartSec=5
-ExecStart=/usr/bin/gst-launch-1.0 udpsrc port=46000 buffer-size=65536 ! \
+#!/usr/bin/env bash
+
+gst-launch-1.0 udpsrc port=46000 buffer-size=65536 ! \
     application/x-rtp,media=audio,payload=127,clock-rate=48000,encoding-name=L16,channels=2 ! \
     rtpjitterbuffer latency=200 ! \
     rtpL16depay ! \
     audioconvert ! \
     pulsesink stream-properties="device=extend_2.0_4.1"
-
-[Install]
-WantedBy=default.target
